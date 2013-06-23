06f84c2cc1f71ee22e7bdf92b2f6eb2a0ea8a93c
Edward Saxton <eddsax@googlemail.com>
2013-06-23 22:06:11 +0000

    Poll yahoo for stock data

--- Python_Examples/PiLiteJson.py
+++ Python_Examples/PiLiteJson.py
@@ -33,8 +33,28 @@ class WeatherPoll(JSONPoll):
         return super(WeatherPoll, self).mung_data(data)
 
 
+class StockPoll(JSONPoll):
+    def __init__(self, stock, message_format=""):
+        """ Encoding a space in the url a + breaks YQL, so requests params
+            handling can't be used
+        """
+        default_format = "{symbol} {DaysLow},{DaysHigh}"
+	base_url = "http://query.yahooapis.com/v1/public/yql"
+        querystring = "select * from yahoo.finance.quotes where symbol='%s'"
+        fmt = "format=json"
+        env = "env="+urllib.quote("store://datatables.org/alltableswithkeys")
+        url = "%s?q=%s&%s&%s"%(base_url, urllib.quote(querystring%stock), fmt, env)
+	super(StockPoll, self).__init__(url, message_format or default_format)
+
+    def mung_data(self, data):
+        """ Drill down to the result to make message_format more readable"""
+        return super(StockPoll, self).mung_data(data['query']['results']['quote'])
+
+
+
 def main():
-    source = WeatherPoll("London,uk")
+    #source = WeatherPoll("London,uk")
+    source = StockPoll("MSFT")
     sink = PiLiteBoard()
     print("ready")
     sink.write("ready  ")
