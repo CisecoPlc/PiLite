cf61a62e1b5e67a3e9553afb93e18092a5cfd725
Edward Saxton <eddsax@googlemail.com>
2013-06-25 11:48:52 +0000

    Fix url encoding for yahoo stock api

--- Python_Examples/PiLiteStock.py
+++ Python_Examples/PiLiteStock.py
@@ -9,17 +9,16 @@ class StockPoll(JSONPoll):
         """ Encoding a space in the url a + breaks YQL, so requests params
             handling can't be used
         """
-        default_format = "{symbol} {DaysLow},{DaysHigh}"
+        default_format = "{symbol} {Bid},{PercentChange}"
 	base_url = "http://query.yahooapis.com/v1/public/yql"
-        querystring = "select * from yahoo.finance.quotes where symbol='%s'"
+        querystring = "from yahoo.finance.quotes where symbol='%s'"
         fmt = "format=json"
-        env = "env="+urllib.quote("store://datatables.org/alltableswithkeys")
-        url = "%s?q=%s&%s&%s"%(base_url, urllib.quote(querystring%stock), fmt, env)
+        env = "env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+        url = "%s?q=select%%20*%%20%s&%s&%s"%(base_url, urllib.quote(querystring%stock), fmt, env)
 	super(StockPoll, self).__init__(url, message_format or default_format)
 
     def mung_data(self, data):
         """ Drill down to the result to make message_format more readable"""
-        print data
         return super(StockPoll, self).mung_data(data['query']['results']['quote'])
 
 
